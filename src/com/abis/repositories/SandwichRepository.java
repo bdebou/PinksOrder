4b518378743b611fefb78b1078c7fc79d8dafb53
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-12 14:44:31 +0200

    Add interface

--- src/com/abis/repositories/SandwichRepository.java
+++ src/com/abis/repositories/SandwichRepository.java
@@ -1,4 +1,15 @@
 package com.abis.repositories;
 
-public  class SandwichRepository extends CommonRepository{
+import com.abis.models.sandwiches.Sandwich;
+import com.abis.repositories.exceptions.SandwichAlreadyExistsException;
+import com.abis.repositories.exceptions.SandwichNotFoundException;
+
+import java.util.List;
+
+public interface SandwichRepository {
+    void addSandwich(Sandwich sandwich) throws SandwichAlreadyExistsException;
+
+    Sandwich getSandwichByName(String name) throws SandwichNotFoundException;
+
+    List<Sandwich> getAll();
 }
