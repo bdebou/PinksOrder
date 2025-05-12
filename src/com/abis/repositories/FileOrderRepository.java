c21ac06289e6c738cab23c937ee77db98185ea34
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-12 14:35:34 +0200

    Add interface

---
+++ src/com/abis/repositories/FileOrderRepository.java
@@ -0,0 +1,40 @@
+package com.abis.repositories;
+
+import com.abis.models.Order;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FileOrderRepository implements OrderRepository{
+    private String fileLocation = "C:\\temp\\javacourses\\orderhistory.csv";
+
+    public String formatOrderToWrite(Order order) {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy '@' hh:mm a");
+        String timestamp = fmt.format(LocalDateTime.now());
+        StringBuilder stringBuilder = new StringBuilder("");
+        stringBuilder.append(timestamp).append(";")
+                .append(order.getOrderingPerson().firstname).append(";")
+                .append(order.getOrderingPerson().lastname).append(";")
+                .append(order.getSandwiches());
+
+        return (stringBuilder.toString());
+    }
+
+    public void writeOrderHistory(Order order) {
+        Path path = Paths.get(fileLocation);
+        try (BufferedWriter writer =
+                     Files.newBufferedWriter(path, Charset.forName("UTF-8"), StandardOpenOption.APPEND)) {
+            writer.newLine();
+            writer.write(formatOrderToWrite(order));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
