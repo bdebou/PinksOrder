3d4fe66efb9e446a7e6be5162aecd70c72a9c6b0
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-12 14:48:47 +0200

    Add FilePersonRepository and its interface

---
+++ src/com/abis/repositories/FilePersonRepository.java
@@ -0,0 +1,16 @@
+package com.abis.repositories;
+
+import com.abis.models.actors.Instructor;
+import com.abis.models.actors.Person;
+import com.abis.models.actors.Student;
+
+public class FilePersonRepository implements PersonRepository{
+    Person instructor = new Instructor("Sandy","Schillebeeckx","JAVA Advanced SE");
+    Person student1 = new Student("Raghunath", "Singh", "TCS");
+    Person student2 = new Student("Bruno", "Deboubers", "ING");
+    Person student3 = new Student("Hans", "Decat", "Contraste");
+    Person student4 = new Student("Tushar", "Pandey", "Cognizant");
+
+
+
+}
--- src/com/abis/repositories/PersonRepository.java
+++ src/com/abis/repositories/PersonRepository.java
@@ -1,16 +1,4 @@
 package com.abis.repositories;
 
-import com.abis.models.actors.Instructor;
-import com.abis.models.actors.Person;
-import com.abis.models.actors.Student;
-
-public class PersonRepository extends CommonRepository{
-    Person instructor = new Instructor("Sandy","Schillebeeckx","JAVA Advanced SE");
-    Person student1 = new Student("Raghunath", "Singh", "TCS");
-    Person student2 = new Student("Bruno", "Deboubers", "ING");
-    Person student3 = new Student("Hans", "Decat", "Contraste");
-    Person student4 = new Student("Tushar", "Pandey", "Cognizant");
-
-
-
+public interface PersonRepository {
 }
