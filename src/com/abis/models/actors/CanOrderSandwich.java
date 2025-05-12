27cbf29cf876dc830d06d934853077a4867c70a4
raghuanand89 <raghuanand89@gmail.com>
2025-05-12 12:05:16 +0200

    updated actors

--- src/com/abis/models/actors/CanOrderSandwich.java
+++ src/com/abis/models/actors/CanOrderSandwich.java
@@ -3,7 +3,11 @@ package com.abis.models.actors;
 import com.abis.models.Order;
 
 public class CanOrderSandwich extends Person {
-    public Order order(){
+    public CanOrderSandwich(String firstname, String lastname, String email) {
+        super(firstname, lastname, email);
+    }
 
+    public CanOrderSandwich(String firstname, String lastname) {
+        super(firstname, lastname);
     }
 }
--- src/com/abis/models/actors/Person.java
+++ src/com/abis/models/actors/Person.java
@@ -5,4 +5,38 @@ public class Person {
     public String lastname;
     public String email;
 
+    public Person(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
