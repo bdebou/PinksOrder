a4ee8465eb6e4541e2f97a3ea7157b132af10daa
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-13 14:12:30 +0200

    Correct CanOrderSandwich to be abstract

--- src/com/abis/models/actors/CanOrderSandwich.java
+++ src/com/abis/models/actors/CanOrderSandwich.java
@@ -1,13 +1,7 @@
 package com.abis.models.actors;
 
-import com.abis.models.Order;
-
-public class CanOrderSandwich extends Person {
+public abstract class CanOrderSandwich extends Person {
     public CanOrderSandwich(String firstname, String lastname, String email) {
         super(firstname, lastname, email);
-    }
-
-    public CanOrderSandwich(String firstname, String lastname) {
-        super(firstname, lastname);
     }
 }
