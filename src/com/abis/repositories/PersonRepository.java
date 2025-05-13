e0bd0db9f00772eda7c9bffdcad4a9bb6e8227ec
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-13 10:15:03 +0200

    Declare all needed methods for PersonRepository

--- src/com/abis/repositories/PersonRepository.java
+++ src/com/abis/repositories/PersonRepository.java
@@ -1,4 +1,14 @@
 package com.abis.repositories;
 
+import com.abis.models.actors.Person;
+
+import java.util.List;
+
 public interface PersonRepository {
+    Person getPersonByName(String firstName, String lastName);
+    List<Person> getAll();
+    Person getPersonByEmail(String email);
+    void addPerson(Person person);
+    void removePerson(Person person);
+
 }
