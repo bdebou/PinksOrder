42ce7f44acc2c05ebb0c38ec03f3ae935ea89e3e
Bruno Deboubers <bruno.deboubers@gmail.com>
2025-05-13 15:05:57 +0200

    PersonService implemented

--- src/com/abis/services/PersonService.java
+++ src/com/abis/services/PersonService.java
@@ -1,4 +1,21 @@
 package com.abis.services;
 
+import com.abis.models.actors.Person;
+import com.abis.repositories.UnitOfWork;
+import com.abis.repositories.exceptions.PersonNotFoundException;
+
 public class PersonService {
+    private final UnitOfWork uow;
+
+    public PersonService(UnitOfWork uow) {
+        this.uow = uow;
+    }
+
+    public Person getPersonByEmail(String mail) throws PersonNotFoundException {
+        return this.uow.getPersonRepository().getPersonByEmail(mail);
+    }
+
+    public Person getPersonByName(String firstName, String lastName) throws PersonNotFoundException {
+        return this.uow.getPersonRepository().getPersonByName(firstName, lastName);
+    }
 }
